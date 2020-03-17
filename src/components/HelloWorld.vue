4c6b926dce2a04f8d11d24853b4385869658eb13
lazantsy <christophe.lazantsy@fenty.com>
2020-03-17 11:49:58 +0100

    add files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,55 +1,71 @@
 <template>
-  <div>
-   <form class="login" @submit.prevent="login">
-     <h1>Sign in</h1>
-     <label>User name</label>
-     <input required v-model="username" type="text" placeholder="Snoopy"/>
-     <label>Password</label>
-     <input required v-model="password" type="password" placeholder="Password"/>
-     <hr/>
-     <button type="submit">Login</button>
-   </form>
- </div>
+  <v-container>
+    <v-form v-model="valid">
+      <v-layout text-center wrap>
+        <v-flex xs12 mb-4>
+          <h1 class="display-2 font-weight-bold mb-3">Sign in</h1>
+        </v-flex>
+        <v-flex xs12>
+            <v-text-field
+              v-model="username"
+              :rules="[rules.username]"
+              :counter="10"
+              label="Username"
+              hint="Enter a Username"
+            ></v-text-field>
+            <v-text-field
+              v-model="password"
+              :counter="10"
+              label="password"
+              hint="Your password passed! Password rules are not meant to be broken!"
+              :append-icon="value ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="() => (value = !value)"
+              :type="value ? 'password' : 'text'"
+              :rules="[rules.password]"
+            ></v-text-field>
+        </v-flex>
+        <v-flex xs12>
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+            @click="login"
+          > Enter </v-btn>
+        </v-flex>
+      </v-layout>
+    </v-form>
+  </v-container>
 </template>
 
 <script>
 import { AUTH_REQUEST } from '../store/actions/auth'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data () {
-    return {
-      username: '',
-      password: ''
+
+  data: () => ({
+    valid: false,
+    value: true,
+    username: '',
+    password: '',
+    rules: {
+      required: value => !!value || 'Required',
+      username: value => {
+        return value.length <= 10 || 'UserName must be less 10 characters'
+      },
+      password: value => {
+        const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/
+        return (
+          pattern.test(value) ||
+            'Min. 8 characters with at least one capital letter, a number and a special character.'
+        )
+      }
     }
-  },
+  }),
   methods: {
-    login: () => {
+    login: function () {
       const { username, password } = this
-      this.$store.dispatch(AUTH_REQUEST, { username, password }).then(() => {
-        this.$router.push('/')
-      })
+      this.$store.dispatch(AUTH_REQUEST, { username, password })
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,70 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {
+  AUTH_REQUEST,
+  AUTH_SUCCESS,
+  AUTH_ERROR,
+  AUTH_LOGOUT
+} from '../store/actions/auth'
+import createLogger from '../plugins/logger'
+const URL_API = 'http://localhost:3000'
 
 Vue.use(Vuex)
+const debug = process.env.NODE_ENV !== 'production'
 
 export default new Vuex.Store({
+  strict: debug,
+  plugins: debug ? [createLogger()] : [],
   state: {
+    token: localStorage.getItem('user-token') || '',
+    status: ''
   },
   mutations: {
+    [AUTH_REQUEST]: state => {
+      state.status = 'loading'
+    },
+    [AUTH_SUCCESS]: (state, token) => {
+      state.status = 'success'
+      state.token = token
+    },
+    [AUTH_ERROR]: state => {
+      state.status = 'error'
+    }
   },
   actions: {
+    [AUTH_REQUEST] ({ commit, dispatch }, user) {
+      commit(AUTH_REQUEST)
+      return new Promise((resolve, reject) => {
+        Vue.axios({
+          url: `${URL_API}/auth/login`,
+          data: user,
+          method: 'POST',
+          useCredentails: true,
+          headers: {
+            'Content-type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+          }
+        })
+          .then(resp => {
+            const token = resp.data.access_token
+            localStorage.setItem('user-token', token)
+            commit(AUTH_SUCCESS, token)
+            resolve(resp)
+          })
+          .catch(err => {
+            commit(AUTH_ERROR, err)
+            localStorage.removeItem('user-token') // if the request fails, remove any possible user token if possible
+            reject(err)
+          })
+      })
+    },
+    [AUTH_LOGOUT]: ({ commit, dispatch }) => {
+      return new Promise((resolve, reject) => {
+        commit(AUTH_LOGOUT)
+        localStorage.removeItem('user-token') // clear your user's token from localstorage
+        resolve()
+      })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
